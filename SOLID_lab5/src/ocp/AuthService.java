e7cca4d223e7b96a4361c62835c8da378522a4e7
ehu <ehu@158.227.124.31>
2025-10-07 11:38:19 +0200

    aldaketa berria

--- SOLID_lab5/src/ocp/AuthService.java
+++ SOLID_lab5/src/ocp/AuthService.java
@@ -1,32 +1,12 @@
 package ocp;
 
 public class AuthService {
-public boolean signIn(String service, String log, String pass) {
-if (service.compareTo("facebook")==0)
-return singInWithFB(log, pass);
-if (service.compareTo("google")==0)
-return singInWithGoogle(log, pass);
-if (service.compareTo("twitter")==0)
-return singInWithTwitter(log, pass);
-if(service.compareTo("Apple")==0)
-return singInWithApple(log,pass);
-return false;
-}
-
-
-public boolean singInWithFB(String log, String pass) {
-//use the FB api
-return true;
-}
-public boolean singInWithGoogle(String log, String pass) {
-//use the google api
-return true;
-}
-public boolean singInWithTwitter(String log, String pass) {
-//use the Twitter api
-return true;
-}
-public boolean singInWithApple(String log,String pass) {
-	return true;
-}
+	private IService service;
+	
+	public AuthService(IService service) {
+		this.service=service;
+	}
+	public boolean singInWithApple(String log,String pass) {
+		return true;
+	}	
 }
